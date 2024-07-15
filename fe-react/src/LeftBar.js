7f74517f84babf4f82b33ca89a9e1d96146e068e
Patrick Goulding <patrickgoulding@gmail.com>
2024-07-15 16:18:50 -0700

    Left and Right Bar logic and styling barebones

--- fe-react/src/LeftBar.js
+++ fe-react/src/LeftBar.js
@@ -1,7 +1,28 @@
-function LeftBar(){
+import { useState } from "react";
+
+function LeftBar({handleClick}){
+    const [num, setNum] =useState('')
+
+    const verifyNum = () => {
+        const guessAsNumber = Number(num)
+        if(guessAsNumber>=0) {
+            handleClick(num)
+        } else {
+            alert('You must guess higher than 0')
+        }
+    }
+
     return (
         <div className="Left-Bar">
-
+            <input 
+                placeholder="Pick a number" 
+                type="number"
+                value={num}
+                onChange={(e) => setNum(e.target.value)}
+            />
+            <button onClick={()=> verifyNum()} >
+                Submit Guess
+            </button>
         </div>
     )
 }
--- fe-react/src/RightBar.js
+++ fe-react/src/RightBar.js
@@ -1,7 +1,17 @@
-function RightBar(){
+import { useState } from "react";
+
+function RightBar({guesses}){
     return (
         <div className="Right-Bar">
-
+            <ol>
+            {  guesses.length != 0 &&
+                    guesses.map(eachGuess => {
+                    return (
+                        <li>Guess:{eachGuess.guessVal} Status:{eachGuess.status}</li>
+                    )
+                })                
+            }
+            </ol>
         </div>
     )
 }
