6d284842cd0caa2013063f3c749b247dbcc49429
Patrick Goulding <patrickgoulding@gmail.com>
2024-07-15 16:18:03 -0700

    Add components and some logic for handling guesses

--- fe-react/src/App.js
+++ fe-react/src/App.js
@@ -2,8 +2,21 @@ import logo from './styling/coin.svg';
 import './styling/App.css';
 import LeftBar from './LeftBar';
 import RightBar from './RightBar';
+import { useState } from 'react';
 
 function App() {
+  const [guesses, setGuesses] = useState([])
+  
+  function handleClick(inputVal) {
+    // fetch to the Back End to calculate and return
+    // fetch(/api/v1/checkguess)
+
+    //mock out for now
+    let guessStatus = 'High'
+    let guessObj = {guessVal:inputVal, status:guessStatus}
+    setGuesses([...guesses, guessObj])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,8 +24,8 @@ function App() {
         <h1>Number Guesser</h1>
       </header>
       <main className="Bar-container">
-        <LeftBar/>
-        <RightBar/>
+        <LeftBar handleClick={handleClick}/>
+        <RightBar guesses={guesses} />
       </main>
     </div>
   );
